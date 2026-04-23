443828de5a709b2cf5a730b15145ef932cd92433
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-23 23:50:53 +0200

    Update dashboard.spec.ts

--- openadmin/tests/dashboard.spec.ts
+++ openadmin/tests/dashboard.spec.ts
@@ -14,6 +14,48 @@ test('access dashboard', async ({ page }) => {
 });
 
 
+
+test('sidebar', async ({ page }) => {
+  await navigateToDashboardPage(page);
+
+  const html = page.locator('html');
+  const sidebar = page.locator('#sidebar');
+
+  await expect(html).toHaveClass(/sidebar-open/);
+  await expect(sidebar).toBeVisible();
+
+  await page.locator('#sidebar_toggle_button').click();
+  await expect(html).not.toHaveClass(/sidebar-open/);
+  await expect(sidebar).not.toBeVisible();
+
+  await page.locator('#sidebar_toggle_button').click();
+  await expect(html).toHaveClass(/sidebar-open/);
+  await expect(sidebar).toBeVisible();
+
+  console.log('sidebar toggle working');
+});
+
+
+test('dark mode', async ({ page }) => {
+  await navigateToDashboardPage(page);
+
+  await page.locator('#user-btn-info').click();
+
+  await expect(page.locator('#theme-toggle-dark-icon')).toBeVisible();
+
+  await page.locator('#theme-toggle-dark-icon').click();
+  await expect(page.locator('#theme-toggle-light-icon')).toBeVisible();
+  await expect(page.locator('html')).toHaveClass(/dark/);
+
+  await page.locator('#theme-toggle-light-icon').click();
+  await expect(page.locator('#theme-toggle-dark-icon')).toBeVisible();
+  await expect(page.locator('html')).not.toHaveClass(/dark/);
+
+  console.log('Dark mode switch is working');
+});
+
+
+
 test('/json/system', async ({ page }) => {
   await page.goto(`/json/system`);
   await expect(page).toHaveURL(/json\/system/);
