a14992b9c430c7e8948e87fe9290f7a5253775f0
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-23 23:17:07 +0200

    Update general_settings.spec.ts

--- openadmin/tests/general_settings.spec.ts
+++ openadmin/tests/general_settings.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('update proxy and test restart needed msg', async ({ page }) => {
   await page.goto(`/settings\/general/`);
   await expect(page).toHaveURL(/settings\/general/);
   await page.getByRole('textbox', { name: 'openpanel' }).fill('newlink');
@@ -15,6 +15,8 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Restart openpanel', exact: true }).click();
   await expect(page.getByText(/1 service needs restart/i)).toBeVisible();
   await page.getByRole('button', { name: 'Restart admin' }).click();
-  await expect(page.getByText(/failed to restar/i)).toBeVisible(); //TODO
-
+  await expect(page.getByText(/failed to restar/i)).toBeVisible(); //TODO better check, wait and reload!
+  await page.goto(`/services/`);
+  await expect(page).toHaveURL(/services/);
+  await expect(page.getByText(/(need restart|needs restart|restart needed)/i)).not.toBeVisible();
 });
