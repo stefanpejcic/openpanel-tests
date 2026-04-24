f05efbb7099efcda7e535834a5c89810defbbad2
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-24 15:14:05 +0200

    Update users.spec.ts

--- openadmin/tests/users.spec.ts
+++ openadmin/tests/users.spec.ts
@@ -50,7 +50,7 @@ test('test autologin', async ({ page, context }) => {
 });
 
 
-test('list user page', async ({ page }) => {
+test('open single user page', async ({ page }) => {
   await page.goto(`/users/testinguser`);
   await expect(page).toHaveURL(/users\/testinguser/);
 
@@ -73,6 +73,130 @@ test('list user page', async ({ page }) => {
   console.log('single user page working');
   // todo: /get_resource_usage_history/testinguser
 });
+
+
+test('test tabs on single user', async ({ page }) => {
+  await page.goto(`/users/testinguser`);
+  await expect(page).toHaveURL(/users\/testinguser/);
+
+  // SERVICES
+  await page.getByRole('link', { name: /services/i }).click();
+  await expect(page).toHaveURL(/#services/);
+
+  const expectedServices = [
+    /name/i, /cpu/i, /memory/i, /actions/i, /disabled/i, /allocated/i,
+  ];
+
+  for (const service of expectedServices) {
+    await expect(page.getByText(service)).toBeVisible();
+  }
+
+  console.log('services tab ok');
+
+  // STORAGE
+  await page.getByRole('link', { name: /storage/i }).click();
+  await expect(page).toHaveURL(/#storage/);
+
+  const expectedServices = [
+    /volumes/i, /containers/i, /images/i, /files/i, /testing_user_html_data/i, /php-fpm-8.5/i,
+  ];
+
+  for (const service of expectedServices) {
+    await expect(page.getByText(service)).toBeVisible();
+  }
+
+  console.log('storage tab ok');
+
+  // OVERVIEW
+  await page.getByRole('link', { name: /overview/i }).click();
+  await expect(page).toHaveURL(/#info/);
+
+  const expectedServices = [
+    /username/i, /locale/i, /email address/i, /2fa/i, /uid/i, /ip address/i, /context/i, /geo location/i, /home dir/i, /varnish/i,  /server/i,  /time/i, /message/i
+  ];
+
+  for (const service of expectedServices) {
+    await expect(page.getByText(service)).toBeVisible();
+  }
+
+  console.log('overview tab ok');
+
+  // EDIT
+  await page.getByRole('link', { name: /edit/i }).click();
+  await expect(page).toHaveURL(/#edit/);
+
+  const expectedServices = [
+    /username/i, /email/i, /images/i, /ip address/i, /package/i, /save/i,
+  ];
+
+  for (const service of expectedServices) {
+    await expect(page.getByText(service)).toBeVisible();
+  }
+
+  console.log('edit tab ok');
+
+  // TRANSFER
+  await page.getByRole('link', { name: /transfer/i }).click();
+  await expect(page).toHaveURL(/#transfer/);
+
+  const expectedServices = [
+    /server/i, /username/i, /password/i, /live transfer/i, /suspend/i,
+  ];
+
+  for (const service of expectedServices) {
+    await expect(page.getByText(service)).toBeVisible();
+  }
+
+  console.log('transfer tab ok');
+
+  // SUSPEND
+  await page.getByRole('link', { name: /suspend/i }).click();
+  await expect(page).toHaveURL(/#suspend/);
+  
+  await expect(page.getByText('confirm', { exact: false })).toBeVisible();
+  await page.locator('[x-model="confirmationText"]').fill('testinguser');
+  await expect(page.getByRole('button', { name: /suspend account/i })).toBeVisible();
+  
+  console.log('suspend tab ok');
+
+  // DELETE
+  await page.getByRole('link', { name: /delete/i }).click();
+  await expect(page).toHaveURL(/#delete/);
+  
+  await expect(page.getByText('confirm', { exact: false })).toBeVisible();
+  await page.locator('[x-model="confirmationText"]').fill('testinguser');
+  await expect(page.getByRole('button', { name: /delete account permanently/i })).toBeVisible();
+  
+  console.log('delete tab ok');
+
+  // ACTIVITY LOG
+  await page.getByRole('link', { name: /activity log/i }).click();
+  await expect(page).toHaveURL(/#activity/);
+  
+  const expectedServices = [
+    /view raw/i, /download/i, /date/i, /action/i, /ip address/i,
+  ];
+
+  for (const service of expectedServices) {
+    await expect(page.getByText(service)).toBeVisible();
+  }
+  console.log('activity tab ok');
+
+  // LOGIN LOG
+  await page.getByRole('link', { name: /login log/i }).click();
+  await expect(page).toHaveURL(/#logins/);
+  
+  const expectedServices = [
+    /view raw/i, /download/i, /date/i, /country/i, /ip address/i,
+  ];
+
+  for (const service of expectedServices) {
+    await expect(page.getByText(service)).toBeVisible();
+  }
+  console.log('loginlog tab ok');
+
+});
+
 
 
 test('search users', async ({ page }) => {
