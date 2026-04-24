d6197a5cf85c3318068cc53bb4860018dd0f804a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-24 18:55:35 +0200

    Update reboot.spec.ts

--- openadmin/tests/reboot.spec.ts
+++ openadmin/tests/reboot.spec.ts
@@ -2,7 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('reboot', async ({ page }) => {
   await page.goto('/server/reboot');
+
   await page.locator('#reboot_type').selectOption('graceful');
   await page.getByRole('button', { name: 'Reboot Server' }).click();
-  await expect(page.getByText('The system has been rebooted.')).toBeVisible({ timeout: 30_000 });
+
+  await expect(page.locator('#success_reboot')).toBeVisible({ timeout: 30_000 });
+  await expect(page.locator('#success_reboot')).toContainText('The system has been rebooted.');
 });
