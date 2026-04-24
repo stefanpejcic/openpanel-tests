4f9509f0489538938e818b5019309f7eba5f603f
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-24 17:27:58 +0200

    Update users.spec.ts

--- openadmin/tests/users.spec.ts
+++ openadmin/tests/users.spec.ts
@@ -69,7 +69,7 @@ test('open single user', async ({ page }) => {
   ];
 
   for (const item of expectedItems) {
-    await expect(page.locator('li', { hasText: item }).first()).toBeVisible();
+    await expect(page.locator('nav a', { hasText: item })).toBeVisible();
   }
   console.log('single user page working');
   // todo: /get_resource_usage_history/testinguser
