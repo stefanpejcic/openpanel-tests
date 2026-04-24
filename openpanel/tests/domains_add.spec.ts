5dabce3618b91deabc8565b2dd6801bc8466c8da
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2026-04-24 13:59:41 +0200

    Update domains_add.spec.ts

--- openpanel/tests/domains_add.spec.ts
+++ openpanel/tests/domains_add.spec.ts
@@ -5,36 +5,38 @@ test('Domain add', async ({ page }) => {
   await expect(page).toHaveURL(/domains/);
   await page.getByRole('link', { name: 'New Domain', exact: true }).click();
   await expect(page).toHaveURL(/domains\/new/);
-  await page.getByRole('textbox', { name: 'Domain*' }).fill('rasa.rs');
+  await page.getByRole('textbox', { name: 'Domain*' }).fill('wp1.jecmenica.rs');
   await page.getByRole('button', { name: 'Add Domain' }).click();
   await expect(page.getByText(/success/i)).toBeVisible();
   console.log(`domain add successfull`);
 
   await page.goto(`/domains`);
   await Promise.all([
-    expect(page.getByText(/rasa.rs/i)).toBeVisible(),
-    expect(page.getByText(/\/var\/www\/html\/rasa.rs/i)).toBeVisible(),
+    expect(page.getByText(/wp1.jecmenica.rs/i)).toBeVisible(),
+    expect(page.getByText(/\/var\/www\/html\/wp1.jecmenica.rs/i)).toBeVisible(),
   ]);
   console.log(`domain visible`);
   
   await page.goto(`/files`);
   await expect(page).toHaveURL(/files/);
-  await expect(page.getByText(/rasa.rs/i)).toBeVisible();
+  await expect(page.getByText(/wp1.jecmenica.rs/i)).toBeVisible();
   console.log(`document root visible`);
 
   
-  await page.goto(`/domains\/edit-dns-zone\/rasa.rs`);
-  await expect(page).toHaveURL(/domains\/edit-dns-zone\/rasa.rs/);
+  await page.goto(`/domains\/edit-dns-zone\/wp1.jecmenica.rs`);
+  await expect(page).toHaveURL(/domains\/edit-dns-zone\/wp1.jecmenica.rs/);
   await expect(page.getByText(/spf1/i)).toBeVisible();
   console.log(`zone file exists`);
   
-  await page.goto(`/domains\/ssl?domain_name=rasa.rs`);
+
+  await page.goto(`/domains\/vhosts?domain=wp1.jecmenica.rs`);
+  await expect(page.getByText(/index.php/i)).toBeVisible();
+  console.log(`vhost file exists`);
+  
+  await page.goto(`/domains\/ssl?domain_name=wp1.jecmenica.rs`);
   const certData = page.locator('#certData');
   await expect(certData).toBeVisible();
   console.log(`cert file exists`);
 
-  await page.goto(`/domains\/vhosts?domain=rasa.rs`);
-  await expect(page.getByText(/index.php/i)).toBeVisible();
-  console.log(`vhost file exists`);
   
 });
