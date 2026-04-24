c61053e312a48770ed6ba3ed0cde84ccaa8a2593
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-24 18:51:27 +0200

    Update reboot.spec.ts

--- openadmin/tests/reboot.spec.ts
+++ openadmin/tests/reboot.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('reboot', async ({ page }) => {
-  await page.goto('/server/reboot/');
+  await page.goto('/server/reboot');
   await page.locator('#reboot_type').selectOption('graceful');
   await page.getByRole('button', { name: 'Reboot Server' }).click();
   const successMessage = page.getByText('The system has been rebooted.');
