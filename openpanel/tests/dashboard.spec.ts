e2f19dc5d1c679fdd8494d05955e3901484b3e70
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-25 18:52:21 +0200

    Update dashboard.spec.ts

--- openpanel/tests/dashboard.spec.ts
+++ openpanel/tests/dashboard.spec.ts
@@ -1,9 +1,55 @@
 import { test, expect } from '@playwright/test';
 
-
-test('create new file and folder', async ({ page }) => {
+async function navigateToDashboardPage(page: any) {
   await page.goto(`/dashboard`);
+  await expect(page).toHaveURL(/dashboard/);
+}
 
 
-  console.log('dashboard is functional');
+test('access dashboard', async ({ page }) => {
+  await navigateToDashboardPage(page);
+  await expect(page.getByText(/cpu usage/i)).toBeVisible();
+  console.log('Dashboard page is accessible');
+});
+
+
+
+test('sidebar', async ({ page }) => {
+  await navigateToDashboardPage(page);
+
+  const html = page.locator('html');
+  const sidebar = page.locator('#sidebar');
+
+  await expect(html).toHaveClass(/sidebar-open/);
+  await expect(sidebar).toBeVisible();
+
+  await page.locator('#sidebar_toggle_button').click();
+  await expect(html).not.toHaveClass(/sidebar-open/);
+  await expect(sidebar).not.toBeVisible();
+
+  await page.locator('#sidebar_toggle_button').click();
+  await expect(html).toHaveClass(/sidebar-open/);
+  await expect(sidebar).toBeVisible();
+
+  console.log('sidebar toggle working');
+});
+
+
+
+test('dark mode', async ({ page }) => {
+  await navigateToDashboardPage(page);
+
+  await page.locator('#user-btn-info').click();
+
+  await expect(page.locator('#theme-toggle-dark-icon')).toBeVisible();
+
+  await page.locator('#theme-toggle-dark-icon').click();
+  await expect(page.locator('#theme-toggle-light-icon')).toBeVisible();
+  await expect(page.locator('html')).toHaveClass(/dark/);
+
+  await page.locator('#theme-toggle-light-icon').click();
+  await expect(page.locator('#theme-toggle-dark-icon')).toBeVisible();
+  await expect(page.locator('html')).not.toHaveClass(/dark/);
+
+  console.log('Dark mode switch is working');
 });
