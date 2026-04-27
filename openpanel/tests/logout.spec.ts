f8c10744eee60322799c19213109352541c998cf
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-27 16:17:06 +0200

    Update logout.spec.ts

--- openpanel/tests/logout.spec.ts
+++ openpanel/tests/logout.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('logout', async ({ page }) => {
   // LOGOUT
-  await page.getByRole('button', { name: 'User settings' }).click();
+  await page.locator('#user-btn-info').click();
   await page.getByRole('link', { name: 'Sign out' }).click();
 
   await expect(page).toHaveURL(/.*login/);
