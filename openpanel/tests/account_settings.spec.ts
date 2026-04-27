5a21b571b684ce4947425af85f497e610c8ab150
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-27 15:40:51 +0200

    Update account_settings.spec.ts

--- openpanel/tests/account_settings.spec.ts
+++ openpanel/tests/account_settings.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('change email', async ({ page }) => {
+test('email address', async ({ page }) => {
   await page.goto(`/account`);
 
   const newEmail = `user_${Date.now()}@testmail.com`;
@@ -14,5 +14,39 @@ test('change email', async ({ page }) => {
   await expect(emailInput).toHaveValue(newEmail);
 
   console.log(`email changed to: ${newEmail}`);
+});
+
+
+
+test('password', async ({ page }) => {
+  await page.goto(`/account`);
+
+  // CHANGE
+  await page.getByRole('textbox', { name: 'Password* Confirm Password*' }).fill('novipassword');
+  await page.getByRole('textbox', { name: 'Confirm New Password' }).fill('novipassword');
+  
+  await page.getByRole('button', { name: 'Update' }).click();
+  
+  await expect(page.locator('body')).toContainText(/has been changed successfully/i);
+  await page.goto(`/files`);
+  await expect(page).toHaveURL(/.*login/);
+
+  // TEST
+  await page.getByRole('textbox', { name: 'Username' }).fill('testinguser');
+  await page.getByRole('textbox', { name: 'Password' }).fill('novipassword');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  await expect(page).toHaveURL(/.*dashboard/);
+  console.log(`password changed to: ${newEmail} and loigin successfully`);
+
+  // REVERT
+  await page.goto(`/account`);
+
+  await page.getByRole('textbox', { name: 'Password* Confirm Password*' }).fill('testingpassword');
+  await page.getByRole('textbox', { name: 'Confirm New Password' }).fill('testingpassword');
+  
+  await page.getByRole('button', { name: 'Update' }).click();
+  
+  await expect(page.locator('body')).toContainText(/has been changed successfully/i);
 });
 
