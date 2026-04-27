81b648c353ebf00f661d3f780e084c84ff2720f0
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-27 15:38:20 +0200

    Update account_settings.spec.ts

--- openpanel/tests/account_settings.spec.ts
+++ openpanel/tests/account_settings.spec.ts
@@ -3,15 +3,16 @@ import { test, expect } from '@playwright/test';
 test('change email', async ({ page }) => {
   await page.goto(`/account`);
 
-  const newEmail = 'stefan@noviemail.rs';
+  const newEmail = `user_${Date.now()}@testmail.com`;
+
   const emailInput = page.getByRole('textbox', { name: 'Email address*' });
+
   await emailInput.fill(newEmail);
   await page.getByRole('button', { name: 'Update' }).click();
 
   await expect(page.locator('body')).toContainText(/has been changed successfully/i);
   await expect(emailInput).toHaveValue(newEmail);
 
-  console.log(`email changed`);
+  console.log(`email changed to: ${newEmail}`);
 });
-
 
