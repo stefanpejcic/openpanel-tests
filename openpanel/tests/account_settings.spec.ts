ed1806de22f3ad6285a8865d46fbf36736a47116
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-27 15:43:24 +0200

    Update account_settings.spec.ts

--- openpanel/tests/account_settings.spec.ts
+++ openpanel/tests/account_settings.spec.ts
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('email address', async ({ page }) => {
-  await page.goto(`/account`);
+  await page.goto('/account');
 
-  const newEmail = `user_${Date.now()}@testmail.com`;
+  const newEmail = `user_${Date.now()}@openpanel.org`;
 
-  const emailInput = page.getByRole('textbox', { name: 'Email address*' });
-
+  const emailInput = page.locator('#email');
   await emailInput.fill(newEmail);
   await page.getByRole('button', { name: 'Update' }).click();
 
@@ -18,39 +17,47 @@ test('email address', async ({ page }) => {
 
 
 
+
 // TODO: username change
 
 
 
 test('password', async ({ page }) => {
-  await page.goto(`/account`);
+  await page.goto('/account');
+
+  const newPassword = 'novipassword';
 
   // CHANGE
-  await page.getByRole('textbox', { name: 'Password* Confirm Password*' }).fill('novipassword');
-  await page.getByRole('textbox', { name: 'Confirm New Password' }).fill('novipassword');
-  
+  await page.locator('#password').fill(newPassword);
+  await page.locator('#confirm_password').fill(newPassword);
+
   await page.getByRole('button', { name: 'Update' }).click();
-  
+
   await expect(page.locator('body')).toContainText(/has been changed successfully/i);
-  await page.goto(`/files`);
+
+  await page.goto('/files');
   await expect(page).toHaveURL(/.*login/);
 
-  // TEST
+  // TEST LOGIN
   await page.getByRole('textbox', { name: 'Username' }).fill('testinguser');
-  await page.getByRole('textbox', { name: 'Password' }).fill('novipassword');
+  await page.locator('#password').fill(newPassword);
   await page.getByRole('button', { name: 'Sign In' }).click();
 
   await expect(page).toHaveURL(/.*dashboard/);
-  console.log(`password changed to: ${newEmail} and loigin successfully`);
+
+  console.log(`password changed and login successful`);
 
   // REVERT
-  await page.goto(`/account`);
+  await page.goto('/account');
 
-  await page.getByRole('textbox', { name: 'Password* Confirm Password*' }).fill('testingpassword');
-  await page.getByRole('textbox', { name: 'Confirm New Password' }).fill('testingpassword');
-  
+  const oldPassword = 'testingpassword';
+
+  await page.locator('#password').fill(oldPassword);
+  await page.locator('#confirm_password').fill(oldPassword);
+
   await page.getByRole('button', { name: 'Update' }).click();
-  
+
   await expect(page.locator('body')).toContainText(/has been changed successfully/i);
 });
+
 
