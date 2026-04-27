4d1613e419a8e53c39a4e3b8b0881be9df0bddc6
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-04-27 10:49:15 +0200

    Update mysql.spec.ts

--- openpanel/tests/mysql.spec.ts
+++ openpanel/tests/mysql.spec.ts
@@ -246,7 +246,7 @@ test('delete database', async ({ page }) => {
   await page.goto(`/mysql/users`);
   await page.getByRole('button', { name: ' Delete' }).nth(1).click();
   await page.getByRole('button', { name: /confirm/i }).nth(1).click();
-  await expect(page.locator('body')).toContainText(/successfully deleted user stefan_usera/i);
+  await expect(page.locator('body')).toContainText(/successfully deleted a database stefan_baza/i);
 
   console.log('delete user is working');
 });
