bc387fadd8e0887393177844b97887bccd098668
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-05-18 13:36:22 +0200

    Create prepare.sh

---
+++ openpanel/prepare.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+[ -z "$1" ] && { echo "Error: pass license key to the script!"; exit 1; }
+
+# defaults
+LICENSE_KEY="$1"
+PANEL_USERNAME="testinguser"
+PANEL_PASSWORD="testingpassword"
+PANEL_EMAIL="test@test.com"
+
+# INCREASE LIMITS SO TESTS DONT GET BLOCKED
+opencli config update login_ratelimit 100
+
+# ENABLE ALL FEATURES
+for f in basic.txt default.txt; do
+  wget -qO- https://raw.githubusercontent.com/stefanpejcic/openpanel-configuration/refs/heads/main/openadmin/config/features.json | jq -r '.[].name' > "/etc/openpanel/openpanel/features/$f"
+done
+
+# ENABLE ALL MODULES
+sed -i "s/^enabled_modules=.*/enabled_modules=$(paste -sd, /etc/openpanel/openpanel/features/default.txt)/" /etc/openpanel/openpanel/conf/openpanel.config
+
+# INSTALL ALL LOCALES
+opencli locale $(curl -s "https://api.github.com/repos/stefanpejcic/openpanel-translations/contents" | jq -r '.[] | select(.type=="dir") | .name' | tr '\n' ' ')
+
+# ADD LICENSE LICENSE
+opencli license $LICENSE_KEY
+
+# ENABLE EMAILS
+opencli email-server install
+
+# ENABLE FTP
+cd /root && docker compose up -d openadmin_ftp
+
+# CREATE USER FOR TESTS
+opencli user-add "$PANEL_USERNAME" "$PANEL_PASSWORD" "$PANEL_EMAIL" 'Developer plus'
+BASE_URL=$(opencli user-login testinguser | sed -E 's#(https?://[^/]+).*#\1#')
+
+#####################################################
+# info for /root/playwright-test/openpanel/.env
+echo "BASE_URL=$BASE_URL"
+echo "PANEL_USERNAME=$PANEL_USERNAME"
+echo "PANEL_PASSWORD=$PANEL_PASSWORD"
