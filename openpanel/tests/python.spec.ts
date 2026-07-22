5d17188349cf52d387c2330793b3f3443e03a0af
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-07-22 23:50:15 +0200

    Update python.spec.ts

--- openpanel/tests/python.spec.ts
+++ openpanel/tests/python.spec.ts
@@ -59,7 +59,7 @@ test.describe('Python autoinstaller', () => {
     await expect(row).toBeVisible();
     await expect(row.getByText(PYTHON_VERSION)).toBeVisible();
   
-    await row.getByRole('link', { name: 'Manage' }).click();
+    await row.getByRole('link', { name: 'Manage', exact: true }).click();
     await expect(page).toHaveURL(`/website?domain=${DOMAIN}`);
   });
 
