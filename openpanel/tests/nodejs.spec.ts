1b6a2dccd2da845f2ff479b283f6abb82629a3ad
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-07-28 17:41:32 +0200

    Update nodejs.spec.ts

--- openpanel/tests/nodejs.spec.ts
+++ openpanel/tests/nodejs.spec.ts
@@ -2,9 +2,6 @@ import { test, expect } from '@playwright/test';
 
 // https://github.com/stefanpejcic/nodejs-helloworld
 
-test.describe.configure({ mode: 'serial' });
-test.setTimeout(180000);
-
 const DOMAIN = 'nodejs.tests.openpanel.org';
 const APP_NAME = 'nodeaplikacija';
 const PORT = '3000';
