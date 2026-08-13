7d974c689c3163ae19565cf857f6caaa73c54abc
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-08-13 19:38:58 +0200

    Update z-cache.spec.ts

--- openpanel/tests/z-cache.spec.ts
+++ openpanel/tests/z-cache.spec.ts
@@ -158,7 +158,7 @@ if ($res === false) {
 
 for (const service of services) {
   test(service.name, async ({ page }) => {
-    test.setTimeout(30_000); // 30s so we also do the healthcheck
+    test.setTimeout(60_000); // 60s so we also do the healthcheck
   
     await navigateToPage(page, service.name);
 
