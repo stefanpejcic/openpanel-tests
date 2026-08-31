9eb227cc0fdab64123122e6b7703b02cc7930f35
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-08-31 12:39:37 +0200

    Update nodejs.spec.ts

--- openpanel/tests/nodejs.spec.ts
+++ openpanel/tests/nodejs.spec.ts
@@ -52,8 +52,8 @@ test.describe('Node.js autoinstaller', () => {
   });
 
   test('2. install app', async ({ page }) => {
-    test.setTimeout(180000);
-
+    test.setTimeout(300000);
+ 
     await page.goto('/nodejs/install');
 
     await page.locator('#service_name').fill(APP_NAME);
