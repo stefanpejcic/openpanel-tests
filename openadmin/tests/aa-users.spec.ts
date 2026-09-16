2fdf1dbb3a6ed93b06bd1c43c9192e27e0028583
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2026-09-16 17:36:51 +0200

    Update success message text in user creation test

--- openadmin/tests/aa-users.spec.ts
+++ openadmin/tests/aa-users.spec.ts
@@ -520,7 +520,7 @@ test('recreate user', async ({ page }) => {
 
   await page.click('#CreateUserButton');
 
-  const successMessage = page.getByText('user created successfully');
+  const successMessage = page.getByText('Successfully added user');
   await expect(successMessage).toBeVisible({ timeout: 120_000 });
 
   console.log('User recreated successfully');
