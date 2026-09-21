e65a3a916ae46bc31e11b0ecce1f567bd796087d
Radovan Ječmenica <147708271+radovanjecmenica@users.noreply.github.com>
2026-09-21 08:01:54 +0200

    Update PHP versions in test file

--- openpanel/tests/php.spec.ts
+++ openpanel/tests/php.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, Page } from '@playwright/test';
 
 const domain = 'wp.tests.openpanel.org';
 
-const versions = [ '8.5', '8.4', '8.3', '8.2', '8.1', '8.0', '7.4', '7.3', '7.2', '7.1', '7.0', '5.6' ];
+const versions = [ '8.6', '8.5', '8.4', '8.3', '8.2', '8.1', '8.0', '7.4', '7.3', '7.2', '7.1', '7.0', '5.6' ];
 
 test.setTimeout(10 * 60 * 1000); // 10min so we can also download docker images
 
